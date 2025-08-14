f7314b28e26bb43655524de1431c17479d050c75
Bala-Subramanyam <gvbalu555@gmail.com>
2025-08-15 00:12:01 +0530

    backend revisons2

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,12 +1,15 @@
 const express=require('express');
 const route=express.Router();
 const {registerAccount,loginAccount} = require('../controllers/authControllers.js');
-
+const authenticate=require('../middleware/jwt_authentication.js')
 
 
 route.post('/register',registerAccount);
 route.post('/login',loginAccount);
-
+route.get('/me',authenticate,(req,res)=>{
+    username=req.user.username;
+    res.json({value:true,user:username});
+});
 
 
 module.exports=route;
